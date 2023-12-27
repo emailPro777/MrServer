cba39d97607ed0e0b8d1929c811afba9a907f128
emailPro777 <154885671+emailPro777@users.noreply.github.com>
2023-12-27 14:19:34 +0200

    Create bw_mserv.py

---
+++ bw_mserv.py
@@ -0,0 +1,8 @@
+from PIL import Image
+from customtkinter import CTkImage
+
+description = '''Полностью свободный от анти-чита сервер, от которого ваша игра\n будет приятной и мощной. БедВарс Сервер, это то что вы так долго искали!'''
+name = 'JBW'
+port = '36766'
+ip = 'BedWarsPro2024.aternos.me'
+icon = CTkImage(Image.open('bw.jpeg'), size=(64, 64))
